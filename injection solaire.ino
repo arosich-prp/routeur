8110ba19e263612a53372f1e654c8c7d5add0b88
arosich-prp <55976649+arosich-prp@users.noreply.github.com>
2022-05-13 10:03:25 +0200

    Update injection solaire.ino

--- injection solaire.ino
+++ injection solaire.ino
@@ -40,24 +40,62 @@
     lcd.setCursor(0, 0);
     lcd.print("Surplus PV injection EDF");
     lcd.setCursor(0,1);
-    lcd.print("        ");
+    lcd.print("          Watts");
     lcd.setCursor(0,1);
-    lcd.print(Puissance());
+    lcd.print(Puissance());    
     lcd.setCursor(0, 2);
     lcd.print("Injection Cumulus");
     lcd.setCursor(0, 3);
     lcd.print(Variateur());
+    lcd.Print(" %");
   }  
   
 //réglage du variateur et renvoie de la valeur:
-  int Puissance(){
-    if (
-    {
-      Variateur.setPower(  ); 
-      //sortie sur le moniteur série du PC:
-     
-      Variateur.getPower();
-       
-     
-    }
+int Variateur(){
+  int niveau=0;
+  int injection=invertion();  
+  if (injection<0){
+    niveau+=2;
   }
+  else {
+    niveau-=2;
+  }
+  if (niveau<0){
+    niveau=0;
+  }
+  elif (niveau>100){
+    niveau=100;
+  }
+  Variateur.setPower(niveau);
+  return Variateur.getPower();
+}
+
+//écoute de la puissance injectée sur le réseau edf pendant 2 secondes (analogRead prend environ 100µs):      
+int Puissance(){
+  int lecture=0;
+  int k;
+  for (k=0;k<20000;k+=1){
+    int l=analogRead(A1);
+    if (l>lecture){
+      lecture=l;
+    }
+  //calcul de la puissance à partir d'un étalonnage sur excel (ici, R=40 ohms et le tor à un rapport de 1000):
+  return (lecture-511)*20;
+}
+
+  //ecoute pour voir s'il y a du courant injecté sur le réseau:
+  int inversion(){
+    int courant1[3];
+    int courant2[3];
+    while(signe1<0 || signe2<0){
+      for (i=0;i<3;i++){
+        courant1[i]=analogread(A1);
+        courant2[i]=analogRead(A2);
+        }
+      int variation1=(courant1[1]-courant1[0]);
+      int variation2=(courant2[1]-courant2[0]);
+      int signe1=(courant1[1]-courant1[0])*(courant1[2]-courant1[1]);
+      int signe2=(courant2[1]-courant2[0])*(courant2[2]-courant2[1]);
+    }
+  return (variation1*variation2);
+}

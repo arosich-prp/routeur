9516566c00a922cff9c2d3c9716281b22d0d8b06
arosich-prp <55976649+arosich-prp@users.noreply.github.com>
2022-06-24 15:47:20 +0200

    Update routeur_v2.ino

--- routeur_v2.ino
+++ routeur_v2.ino
@@ -116,9 +116,9 @@
 
 //fonction de lecture plus sécurisée, elle prend environ 20 ms
  int safeAnalogRead(int pin) {
-     delay(9.89);   
+     delayMicroseconds(9890);   
      int x = analogRead(pin);  //première lecture pour initialiser l'entrée de l'ADC
-     delay(9.89);              //délai de stabilisation du port
+     delayMicroseconds(9890);              //délai de stabilisation du port
      x = analogRead(pin);      //lecture sur le port de l'ADC, environ 110µs=0,11ms
      // retour à 20ms
      return x;
